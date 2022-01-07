813eafa28210da4061c105e1cbcb53cefa849665
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-06 21:59:41 -0800

    positions from list noted

---
+++ who won.py
@@ -0,0 +1,15 @@
+tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n"
+# did x win, need two functions
+#use find of each and label
+# 1= 0
+# 2 =2
+# 3 =4
+# 4= 7
+# 5= 9
+# 6= 11
+# 7= 13
+# 8= 15
+# 9= 17
+print (tic_tac_toe_chart[17])
+
+# def did_x_win(player_x_name, player_o_name,tic_tac_toe_chart):
