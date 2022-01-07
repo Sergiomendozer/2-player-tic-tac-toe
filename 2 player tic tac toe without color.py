8d5b6b8b39459f07741c9b0db3758d316c89ee1b
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-06 21:38:34 -0800

    edit

---
+++ 2 player tic tac toe without color.py
@@ -0,0 +1,69 @@
+# Red="\033[0;31m"
+# Blue="\033[0;34m"
+# END = '\033[0m'
+def player_x_tic_tac_toe (player_x_name, player_o_name,tic_tac_toe_chart):
+    print(END + tic_tac_toe_chart) 
+    position = input(Blue + player_x_name + ", select a position you would like to place X:")# fix end to be more proper
+    if position == '3': #add notes why this is needed, accounts for blue
+        replace = tic_tac_toe_chart.find(position)
+        spot_of_blue =tic_tac_toe_chart.find("4", replace)
+        spot_of_red = tic_tac_toe_chart.find("1", replace)
+        if spot_of_blue == replace +1:
+            print('bluex')#####D
+            not_blue=tic_tac_toe_chart.find('3', spot_of_blue) 
+            tic_tac_toe_chart = (tic_tac_toe_chart[:not_blue] +"\033[0;34mX\033[0m" +tic_tac_toe_chart[not_blue+1:])
+            return player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+        elif spot_of_red == replace +1:
+            print('redx')#####D
+            not_red=tic_tac_toe_chart.find('3', spot_of_red) 
+            tic_tac_toe_chart = (tic_tac_toe_chart[:not_red] +"\033[0;34mX\033[0m" +tic_tac_toe_chart[not_red+1:])
+            return player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+        else:
+            print("it is okayx") #####D
+            tic_tac_toe_chart = (tic_tac_toe_chart[:replace]+ "\033[0;34mX\033[0m" + tic_tac_toe_chart[replace + 1:])
+            return player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+    else:
+        print ("ELSEx") #####D
+        replace = tic_tac_toe_chart.find(position)
+        replace = tic_tac_toe_chart.find(position)
+        tic_tac_toe_chart = tic_tac_toe_chart[:replace] + Blue + 'X' + END+ tic_tac_toe_chart[replace+1:]
+        return player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+
+    # call after each if ########################
+def player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart):
+    print ("called")
+    print (END + tic_tac_toe_chart)
+    position = input(Red + player_o_name + ", select a position you would like to place O:")
+    replace = tic_tac_toe_chart.find(position)
+    if position == '3': #add notes why this is needed, accounts for blue
+        replace = tic_tac_toe_chart.find(position)
+        spot_of_blue =tic_tac_toe_chart.find("4", replace)
+        spot_of_red = tic_tac_toe_chart.find("1", replace)
+        if spot_of_blue == replace +1:
+            print('blueo')#####D
+            not_blue=tic_tac_toe_chart.find('3', spot_of_blue) 
+            tic_tac_toe_chart = (tic_tac_toe_chart[:not_blue] +"\033[0;31mO\033[0m" +tic_tac_toe_chart[not_blue+1:])
+            return player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+        elif spot_of_red == replace +1:
+            print('redo')#####D
+            not_red=tic_tac_toe_chart.find('3', spot_of_red) 
+            tic_tac_toe_chart = (tic_tac_toe_chart[:not_red] +"\033[0;31mO\033[0m" +tic_tac_toe_chart[not_red+1:])
+            return player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+        else:
+            print("its okayo") #####D
+            tic_tac_toe_chart = (tic_tac_toe_chart[:replace]+ "\033[0;31mO\033[0m" + tic_tac_toe_chart[replace + 1:])
+            return player_o_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+    else:
+        print ("ELSEo") #####D
+        replace = tic_tac_toe_chart.find(position)
+        replace = tic_tac_toe_chart.find(position)
+        tic_tac_toe_chart = tic_tac_toe_chart[:replace] + Red + 'O' + END+ tic_tac_toe_chart[replace+1:]
+        return player_x_tic_tac_toe(player_x_name, player_o_name,tic_tac_toe_chart)
+    # tic_tac_toe_chart = tic_tac_toe_chart[:replace] + Red +"O" + END + tic_tac_toe_chart[replace+1:]
+    # return player_x_tic_tac_toe (player_x_name, player_o_name,tic_tac_toe_chart)
+
+
+player_x_name = str(input(Blue + "Enter player name that wil play X:"))
+player_o_name = str(input(Red + "Enter player name that wil play O:"))
+tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n"
+player_x_tic_tac_toe (player_x_name, player_o_name,tic_tac_toe_chart)
