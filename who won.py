2f683bd93c2120976bc67917092dd7901111b5e9
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-06 22:04:37 -0800

    function being worked on

--- who won.py
+++ who won.py
@@ -1,4 +1,4 @@
-tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n"
+
 # did x win, need two functions
 #use find of each and label
 # 1= 0
@@ -10,6 +10,17 @@ tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n"
 # 7= 13
 # 8= 15
 # 9= 17
-print (tic_tac_toe_chart[17])
+# print (tic_tac_toe_chart[17])
 
-# def did_x_win(player_x_name, player_o_name,tic_tac_toe_chart):
+
+def did_x_win(player_x_name, player_o_name,tic_tac_toe_chart):
+    print ("yellow")
+
+
+
+tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n" 
+tic_tac_toe_chart = "X 2 3 \n4 5 6\n7 8 9\n" 
+print (tic_tac_toe_chart)
+player_x_name = 1 
+player_o_name = 2
+did_x_win(player_x_name, player_o_name,tic_tac_toe_chart)
