f60140dc8bb823d978b9535b03e8527f90a910c3
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-07 13:50:22 -0800

    x winner 1-2-3

--- who won.py
+++ who won.py
@@ -21,14 +21,18 @@ def did_x_win(player_x_name, player_o_name,tic_tac_toe_chart):
     position_seven= tic_tac_toe_chart[13]
     position_eight = tic_tac_toe_chart[15]
     position_nine = tic_tac_toe_chart[17]
-    if position_one == "X":
+    if position_one == "X" and position_two == "X" and position_three == "X":
         print ("yellow")
+    # if position_two == "X":
+    #     print ("yellow")
+    # if position_three == "X":
+    #     print ("yellow")
     else:
         print ('NO one one')
 
 #####################################
-tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n" 
-tic_tac_toe_chart = "X 2 3 \n4 5 6\n7 8 9\n" 
+# tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n" 
+tic_tac_toe_chart = "X X X \n4 5 6\n7 8 9\n" 
 print (tic_tac_toe_chart)
 player_x_name = 1 
 player_o_name = 2
