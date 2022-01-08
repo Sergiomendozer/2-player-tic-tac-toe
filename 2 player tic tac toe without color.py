58d66cfdc3e99d02105ed588f1432ab45adea49e
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-07 17:50:42 -0800

    players can play over and over again, NEXT: add if there is a tie to loop

--- 2 player tic tac toe without color.py
+++ 2 player tic tac toe without color.py
@@ -1,4 +1,4 @@
-RED="\033[0;31m"          # Red
+RED="\033[0;31m"          
 BLUE="\033[0;34m" 
 END = '\033[0m'
 
@@ -37,22 +37,30 @@ def did_x_win(player_x_name, player_o_name,tic_tac_toe_chart):
     # 1 2 3 is filled with X
     if position_one == "X" and position_two == "X" and position_three == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     # 4 5 6 filled with X
     elif position_four == "X" and position_five == "X" and position_six == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     # 7 8 9 filled with X
     elif position_seven == "X" and position_eight == "X" and position_nine == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_one == "X" and position_four == "X" and position_seven == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_two == "X" and position_five == "X" and position_eight == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_three == "X" and position_six == "X" and position_nine == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_one == "X" and position_five == "X" and position_nine == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_three == "X" and position_five == "X" and position_seven == "X":
         print (BLUE + player_x_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     else:
         return tic_tac_toe_player_o (player_x_name, player_o_name,tic_tac_toe_chart)
 
@@ -70,24 +78,37 @@ def did_o_win(player_x_name, player_o_name,tic_tac_toe_chart):
     # 1 2 3 is filled with X
     if position_one == "O" and position_two == "O" and position_three == "O":
         print (RED + player_o_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     # 4 5 6 filled with X
     elif position_four == "O" and position_five == "O" and position_six == "O":
         print (RED + player_o_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     # 7 8 9 filled with X
     elif position_seven == "O" and position_eight == "O" and position_nine == "O":
         print (RED + player_o_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_one == "O" and position_four == "O" and position_seven == "O":
         print (RED + player_o_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_two == "O" and position_five == "O" and position_eight == "O":
         print (RED + player_o_name +" is the winner!" + END)
     elif position_three == "O" and position_six == "O" and position_nine == "O":
         print (RED + player_o_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_one == "O" and position_five == "O" and position_nine == "O":
         print (RED + player_o_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     elif position_three == "O" and position_five == "O" and position_seven == "O":
         print (RED + player_o_name +" is the winner!" + END)
+        return play_again(player_x_name, player_o_name,tic_tac_toe_chart)
     else:
         return tic_tac_toe_player_x (player_x_name, player_o_name,tic_tac_toe_chart)
+
+# lets player play again and again
+def play_again(player_x_name, player_o_name,tic_tac_toe_chart):
+    print ("This is a new game")
+    tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n"
+    tic_tac_toe_player_x (player_x_name, player_o_name,tic_tac_toe_chart)
 
 # prompts users to enter name and start playing game
 player_x_name = str(input("Enter player name that wil play X:"))
--- play again function.py
+++ play again function.py
@@ -1,1 +1,4 @@
-def play_again((player_x_name, player_o_name,tic_tac_toe_chart))
+def play_again(player_x_name, player_o_name,tic_tac_toe_chart):
+    print ("lets play another game")
+    tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n"
+    tic_tac_toe_player_x (player_x_name, player_o_name,tic_tac_toe_chart)
